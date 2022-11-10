b96567ac31d8e23ce2cacf929b9d05056fad8734
paiman Nejrabi <paimannejrabi@campus-034-121.ucdavis.edu>
2022-11-10 15:31:09 -0800

    mount

--- p3/libfs/fs.c
+++ p3/libfs/fs.c
@@ -10,42 +10,56 @@
 /* TODO: Phase 1 */
 
 struct  __attribute__((__packed__)) superBlock {
-	uint8_t signature;
-	uint16_t VD_Block;
+	char	 signature[8];
+	uint16_t total_Block_counter;
 	uint16_t Root_Dir;
 	uint16_t Data_Block;
 	uint16_t Amout_Data_Block;
-	uint8_t  Fat_Number;
+	uint8_t  Fat_block;
 	uint8_t padding [4079];
 };
 
 struct  __attribute__((__packed__)) fatBlock {
-	uint16_t *Fat_array;
+	uint16_t Fat_Block_index;
+	// fat_block_index[1]
 };
 
 struct  __attribute__((__packed__)) rootDir {
-	uint8_t Filename[FS_FILENAME_LEN];
+	char Filename[FS_FILENAME_LEN];
 	uint32_t file_Size;
 	uint16_t index;
 	uint8_t padding[10];
 };
 
-struct superBlock sb;
-struct fatBlock Fb;
-struct rootDir rd;
+
 
 int fs_mount(const char *diskname)
 {
+	struct superBlock *sb = malloc(sizeof(struct superBlock));
+	memeset(sb, 0, BLOCK_SIZE);
+	struct fatBlock *Fb = malloc(sizeof(struct fatBlock));
+	memeset(Fb, 0, BLOCK_SIZE);
+	struct rootDir *rd = malloc (sizeof(struct rootDir));
+	memeset(rd, 0, BLOCK_SIZE);
 	/* TODO: Phase 1 */
 	int open = block_disk_open(diskname);
 	if(open == -1){
-		printf("Unable to open the Disk\n");
 		return -1;
 	}
 	if(block_read(0, &sb) == -1){
-		printf("Could not read the block\n");
+		return -1;
 	}
 	
+	if(!strcomp(sb->signature,"ECS150FS", 8)){
+		return -1;
+	}
+	if(sb->total_Block_counter != block_disk_count()){
+		return -1;
+	}
+	
+
+
+
 
 }
 
@@ -56,7 +70,7 @@ int fs_umount(void)
 
 int fs_info(void)
 {
-	/* TODO: Phase 1 */
+	/* TODO: Phase 1 
 
 	printf("FS info: \n");
 	printf("Virtual Desk %i\n", sb.VD_Block);
@@ -66,6 +80,7 @@ int fs_info(void)
 	printf("Number of Fat %i\n", sb.Fat_Number);
 	printf("Padding %i\n", sb.padding);
 
+*/
 }
 
 int fs_create(const char *filename)
